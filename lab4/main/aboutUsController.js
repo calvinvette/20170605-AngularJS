8e0bac69ea19610dcc614c4c69a14bc802bb167f
calvinvette <calvin@dragontale.com>
2017-06-06 11:42:12 -0400

    Lab 4 Solution

---
+++ lab4/main/aboutUsController.js
@@ -0,0 +1,37 @@
+(function() {
+
+    var app = angular.module("mainModule");
+    app.controller("aboutUsController", controller);
+    controller.$inject = ['$scope'];
+
+    function controller ($scope) {
+        $scope.debug = false;
+
+        $scope.states = [
+            "Florida",
+            "Michigan",
+            "New Jersey",
+            "New York",
+            "North Carolina",
+            "Pennsylvania"
+        ];
+
+
+        console.log("about us controller loaded...");
+        $scope.companyName = "Foo Corp";
+        $scope.address = "1234 Main St.";
+        $scope.city = "Schenectady";
+        $scope.state = "NY";
+        $scope.postalCode = "12345";
+        $scope.phoneNumber = "212-555-1212";
+
+        $scope.date = new Date();
+
+        $scope.register = function () {
+            console.log("Registration not implemented...");
+            console.log($scope.companyName);
+            // debugger;
+        }
+    };
+
+})();
