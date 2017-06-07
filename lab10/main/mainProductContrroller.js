46708b8b6404e74283794183245b47e66be272a9
calvinvette <calvin@dragontale.com>
2017-06-07 17:12:43 -0400

    Lab 10 - added $http to productService and categoryService
    Also updated controllers to use promise-based "then" clauses.

---
+++ lab10/main/mainProductContrroller.js
@@ -0,0 +1,68 @@
+(function() {
+
+
+    var app = angular.module("mainModule");
+    app.controller("mainProductContrroller", controller);
+    controller.$inject = ['$scope', '$http'];
+
+    function controller ($scope, $http) {
+        console.log("$http injection: ");
+        console.log($http);
+        $scope.topProducts = [
+            {
+                "productID" : 56,
+                "productName" : "A fake product",
+                "supplierID" : 8,
+                "categoryID" : 1,
+                "quantityPerUnit" : "Box of 12",
+                "unitPrice" : 81,
+                "unitsInStock" : 40,
+                "unitsOnOrder" : 0,
+                "reorderLevel" : 0,
+                "discontinued" : 0,
+                "featured" : true
+            },
+            {
+                "productID" : 12,
+                "productName" : "Another fake product",
+                "supplierID" : 8,
+                "categoryID" : 3,
+                "quantityPerUnit" : "Box of 12",
+                "unitPrice" : 11.23,
+                "unitsInStock" : 40,
+                "unitsOnOrder" : 0,
+                "reorderLevel" : 0,
+                "discontinued" : 0,
+                "featured" : true
+            },
+            {
+                "productID" : 20,
+                "productName" : "Yet another fake product",
+                "supplierID" : 8,
+                "categoryID" : 2,
+                "quantityPerUnit" : "6 pack",
+                "unitPrice" : 85.44,
+                "unitsInStock" : 40,
+                "unitsOnOrder" : 0,
+                "reorderLevel" : 0,
+                "discontinued" : 0,
+                "featured" : true
+            },
+            {
+                "productID" : 25,
+                "productName" : "A fourth fake product",
+                "supplierID" : 8,
+                "categoryID" : 4,
+                "quantityPerUnit" : "Box of 12",
+                "unitPrice" : 81.34,
+                "unitsInStock" : 40,
+                "unitsOnOrder" : 0,
+                "reorderLevel" : 0,
+                "discontinued" : 0,
+                "featured" : true
+            }
+        ];
+
+    };
+
+})();
